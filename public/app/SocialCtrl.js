c953206efe511e4e3a4963456a696d9253cd0c96
CindyB85 <sio.bodet.cindy@gmail.com>
2015-11-11 12:32:24 +0100

    Lien clicable

--- public/app/SocialCtrl.js
+++ public/app/SocialCtrl.js
@@ -25,17 +25,18 @@ angular
       if( messageIsALink(this.message)){
         SocketIOService.sendLink(this.message);
         SocketIOService.sendMessage(this.message);
+        this.message = '';
       } else {
         console.log(this.message);
         console.log("c'est un message");
         SocketIOService.sendMessage(this.message);
+        this.message = '';
       }
     };
 
     function onMessagesUpdated(message) {
       this.messages.push(message);
       $scope.$apply();
-      this.message = "";
     }
 
     SocketIOService.onMessagesUpdated(onMessagesUpdated.bind(this));
@@ -43,7 +44,6 @@ angular
     function onLinkUpdated(message){
       this.linkPin.push(message);
       $scope.$apply();
-      this.message = "";
     }
 
     SocketIOService.onLinkUpdated(onLinkUpdated.bind(this));
